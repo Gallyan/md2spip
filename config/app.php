53a68770ecaeed7c47db99eb84a1b2907b1140f5
Guillaume Orsal (protovision) <guillaume@orsal.net>
2026-01-12 15:34:13 +0100

    Ajout email de contact dans la config

--- config/app.php
+++ config/app.php
@@ -56,6 +56,18 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Contact Email
+    |--------------------------------------------------------------------------
+    |
+    | This value is the contact email address used in legal notices.
+    | The email is obfuscated with CSS to protect against spam bots.
+    |
+    */
+
+    'contact_email' => env('CONTACT_EMAIL', 'contact@example.com'),
+
+    /*
+    |--------------------------------------------------------------------------
     | Application Timezone
     |--------------------------------------------------------------------------
     |
