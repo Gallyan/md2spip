3d9ad32eec3401536d81fb55ee3b7d4337177182
Guillaume Orsal (protovision) <guillaume@orsal.net>
2026-01-13 11:43:37 +0100

    update workflow

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Livewire\MarkdownToSpipPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', MarkdownToSpipPage::class);
-Route::get('/mentions-legales', fn () => view('mentions-legales')); // @phpstan-ignore argument.type
+Route::get('/mentions-legales', fn () => view('mentions-legales'));
 
 // Redirection email obfusquée (protection anti-spam)
 Route::get('/contact-email', ContactRedirectController::class);
