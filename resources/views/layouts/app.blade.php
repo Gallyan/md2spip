e62f60dc9749be2ede44bfa585ae2b4de164cc87
Guillaume Orsal <guillaume@orsal.net>
2026-05-17 01:01:23 +0200

    evitons le flash blanc à la navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script>
+        if (localStorage.getItem('md2spip-theme') !== 'light') {
+            document.documentElement.classList.add('dark');
+        }
+    </script>
     <title>{{ __('messages.meta.title') }}</title>
     <meta name="description" content="{{ __('messages.meta.description') }}">
     <meta name="keywords" content="{{ __('messages.meta.keywords') }}">
