9167fe3b849437d262246d8e85aa6dba154113cc
Guillaume Orsal <guillaume@orsal.net>
2026-08-20 18:32:18 +0200

    Lien vers orsal.fr dans la bannière console

--- resources/js/console-banner.js
+++ resources/js/console-banner.js
@@ -38,8 +38,9 @@ for (let row = 0; row < lines.length; row++) {
     banner += '\n';
 }
 
-banner += '\n%cConvertisseur Markdown → SPIP\n%cpar Guillaume Orsal';
+banner += '\n%cConvertisseur Markdown → SPIP\n%cpar Guillaume Orsal\n%chttps://www.orsal.fr';
 styles.push('color:#94a3b8;font-size:11px');
 styles.push('color:#10b981;font-size:11px;font-weight:bold');
+styles.push('color:#10b981;font-size:11px;text-decoration:underline');
 
 console.log(banner, ...styles);
