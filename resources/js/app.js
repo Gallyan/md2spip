6868edf2757981de6b2acbc38a8541afe2fb8a35
Guillaume Orsal <guillaume@orsal.net>
2026-09-15 08:22:36 +0200

    Retire axios et alpinejs, jamais utilisés côté front

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,8 @@
-import './bootstrap';
 import './console-banner';
 
-// Note: Alpine est géré automatiquement par Livewire 3
-// Ne pas importer ni démarrer Alpine manuellement
+// Alpine est embarqué par Livewire : ne pas l'importer ni le démarrer ici
 
 // Intercepteur global pour gérer l'expiration de session (erreur 419)
-// API Livewire 3.x : Livewire.hook('request', ...)
 document.addEventListener('livewire:init', () => {
     Livewire.hook('request', ({ fail }) => {
         fail(({ status, preventDefault }) => {
